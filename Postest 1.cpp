aa8c84b2f3cbd5576f1527271c5a4e182d6928e5
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2021-12-14 09:25:32 +0700

    Add files via upload

---
+++ Postest 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string a;
+	string b;
+	string c;
+	
+	cout<<"Masukkan input data a b c : ";
+	cin>>a>>b>>c;
+	
+	cout<<endl<<"Hasil output fungsi : "<<a<<"x^2 + "<<b<<"x + "<<c;
+	
+	return 0;
+}
