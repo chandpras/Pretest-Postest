a696bff9913ec8f495de2cef71c0fea9781d5e66
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2021-12-15 12:04:05 +0700

    Update Postest 2.cpp

--- Postest 2.cpp
+++ Postest 2.cpp
@@ -4,16 +4,13 @@ using namespace std;
 
 int main (){
 	
-	//tipe data
 	int a, b, c, x;
 	
-	//input
 	cout<<"========================================"<<endl;
 	cout<<"Operasi ax^2 + bx + c"<<endl<<endl;
 	cout<<"Masukkan angka a b c x = ";
 	cin>>a>>b>>c>>x;
 	
-	//output
 	cout<<"========================================"<<endl;
 	cout<<"Hasil Operasi = ";
 	cout<<((a*(x*x))+(b*x)+(c));
